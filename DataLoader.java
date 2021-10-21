64b854913e8dc2e14e2a5215f78afa1789260e07
Sobasshi <jjtupacz@gmail.com>
2021-10-21 13:50:27 -0400

    users update

---
+++ DataLoader.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import org.json.simple.JSONArray;
+
+public class DataLoader {
+
+    public static ArrayList<Job> loadJobs() {
+        ArrayList<Job> JobList = new ArrayList<Job>();
+    }
+
+}
