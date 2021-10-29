a5daca25ee973c532a900b2dea292472c2238bfc
law14 <law14@email.sc.edu>
2021-10-29 03:46:17 -0400

    uh facade and UI

---
+++ ProgramFacade.java
@@ -0,0 +1,22 @@
+public class ProgramFacade {
+    private ResumeList resumes = ResumeList.getInstance();
+    private ApplicationList applications = ApplicationList.getInstance();
+    private JobListings jobListings = JobListings.getInstance();
+    private UserList users = UserList.getInstance();
+
+    public ProgramFacade() {
+
+    }
+
+    public User findUser() {
+
+    }
+
+    public Job findJob() {
+
+    }
+
+    public Employer findEmployer() {
+
+    }
+}
---
+++ ProgramUI.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ProgramUI {
+
+
+    public void run() {
+
+    }
+
+    public void displayLogin() {
+
+    }
+
+    public void displayMainMenu() {
+
+    }
+
+    public void displayResumes() {
+
+    }
+
+    public void displayJobListings() {
+
+    }
+
+    public void displayApplicants() {
+
+    }
+
+    public void displayUsers() {
+
+    }
+
+}
