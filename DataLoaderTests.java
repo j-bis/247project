2f8d7c6016e67df12a0b42c06f5221af462a6680
Sobasshi <jjtupacz@gmail.com>
2021-11-14 22:43:34 -0500

    move

--- DataLoaderTests.java
+++ DataLoaderTests.java
@@ -1,3 +1,11 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.beans.Transient;
@@ -8,15 +16,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DataLoaderTests {
+    public UserList userlist;
 
     @BeforeEach
-    public void initialize() {
-        DataLoader.init();
+    public void setup() {
+        JobListings jobListings = JobListings.getInstance();
+        ResumeList resumeList = ResumeList.getInstance();
+        UserList userlist = UserList.getInstance();
+        ApplicationList applicationList = ApplicationList.getInstance();
+
+        ArrayList<Job> jobs = DataLoader.loadJobs();
+        ArrayList<Resume> resumes = DataLoader.loadResumes();
+        ArrayList<User> users = DataLoader.loadUsers();
+        ArrayList<Student> students = DataLoader.loadStudents();
+        ArrayList<Application> apps = DataLoader.loadApplications();
     }
 
     @Test
-    public void getUUIDtest() {
-        assertEquals(expected, actual);
+    public void getUserByUUIDtest() {
+        assertEquals("Aaron Longshore", userlist.getStudentById(UUID.fromString("2d6159e1-46d9-4248-8356-c9576cacf5a1")).getDisplayName());
     }
+
+    @Test
+    public void 
     
 }
