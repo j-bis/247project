0b62185de89b355590b24eff732c02670e0da3df
j-bis <jjtupacz@gmail.com>
2021-11-16 04:26:23 -0500

    test

--- DataLoaderTests.java
+++ DataLoaderTests.java
@@ -17,27 +17,70 @@ import org.junit.jupiter.api.Test;
 
 public class DataLoaderTests {
     public UserList userlist;
+    ArrayList<User> users;
+    ArrayList<Resume> resumes;
+    ArrayList<Student> students;
+    ArrayList<Application> apps;
+    ArrayList<Job> jobs;
+    ApplicationList applicationList;
+    JobListings jobListings;
+    ResumeList resumeList;
 
     @BeforeEach
     public void setup() {
-        JobListings jobListings = JobListings.getInstance();
-        ResumeList resumeList = ResumeList.getInstance();
-        UserList userlist = UserList.getInstance();
-        ApplicationList applicationList = ApplicationList.getInstance();
+        jobListings = JobListings.getInstance();
+        resumeList = ResumeList.getInstance();
+        userlist = UserList.getInstance();
+        applicationList = ApplicationList.getInstance();
 
-        ArrayList<Job> jobs = DataLoader.loadJobs();
-        ArrayList<Resume> resumes = DataLoader.loadResumes();
-        ArrayList<User> users = DataLoader.loadUsers();
-        ArrayList<Student> students = DataLoader.loadStudents();
-        ArrayList<Application> apps = DataLoader.loadApplications();
+        jobs = DataLoader.loadJobs();
+        resumes = DataLoader.loadResumes();
+        users = DataLoader.loadUsers();
+        students = DataLoader.loadStudents();
+        apps = DataLoader.loadApplications();
     }
 
     @Test
-    public void getUserByUUIDtest() {
+    public void testGetUserByUUID() {
         assertEquals("Aaron Longshore", userlist.getStudentById(UUID.fromString("2d6159e1-46d9-4248-8356-c9576cacf5a1")).getDisplayName());
     }
 
     @Test
-    public void 
+    public void testGetUserSize() {
+        assertEquals(6, users.size());
+    }
+
+    @Test
+    public void testGetApplicationIDFromJobID() {
+        assertEquals("44d5cd4b-de75-4261-a213-0c5afc537930", applicationList.getAppByJobID("d6383f24-c509-4ee3-84eb-8bda3946290b").getID());
+    }
+
+    @Test
+    public void testGetJobTitle() {
+        assertEquals("Internship: Graphic Design", jobListings.getJobByUUID(UUID.fromString("d6383f24-c509-4ee3-84eb-8bda3946290b")).getTitle());
+    }
+
+    @Test
+    public void testGetJobsSizeThree() {
+        DataWriter.save();
+        assertEquals(3, jobs.size());
+    }
+
+    @Test
+    public void testAddResume() {
+        resumeList.addResume(new Resume());
+        assertEquals(4, resumeList.getResumes().size());
+    }
     
+    @Test
+    public void testLoadApplications() {
+        assertEquals(3, applicationList.getApplicants().size());
+    }
+
+    @Test
+    public void testLoadUsersAgain() {
+        ArrayList<User> u2 = users;
+        users = DataLoader.loadUsers();
+        assertEquals(u2, users);
+    }
 }
